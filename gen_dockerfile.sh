fa0022d5c4bc7cf785331e511d061285a5c07eb5
Zandy <zandyo@gmail.com>
2017-12-07 23:10:23 +0800

    update Dockerfile

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 cat images|awk '{print "mkdir -p "$1";echo \"FROM "$1":"$2"\" > "$1"/Dockerfile"}'|bash
+cat images|awk '{print "mkdir -p "$1";echo -e \"FROM "$1":"$2"\\nMAINTAINER Zandy\" > "$1"/Dockerfile"}'|bash
 
